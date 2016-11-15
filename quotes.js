329d817443780a09549b571c3b1f841a459d2173
jProgr <8537691@gmail.com>
2016-11-15 00:10:19 +0000

    Button now shows a random quote

--- quotes.js
+++ quotes.js
@@ -1,8 +1,20 @@
+function quote(select)
+{ return json[select].quote; }
+
+function author(select)
+{ return json[select].author; }
+
+var json;
+
+$.getJSON("quotes_list.json", (data) => { json = data; });
+
 $(document).ready(function()
 {
     $("#new_quote").on("click", function()
     {
-        $(".quote_line").html("Here is the message");
+        var pick = (Math.random() * json.length) | 0;
+        $(".quote_line").html(quote(pick) + ".");
+        $(".author_line").html(author(pick) + ".");
     });
 
 });
